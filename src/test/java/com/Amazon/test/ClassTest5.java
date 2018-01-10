12508e5ecf99c22dab99fa04145bd62a2ae1eed0
Bibek0914 <bibek.mallik14@gmail.com>
2018-01-11 00:21:55 +0530

    commited

--- src/test/java/com/Amazon/test/ClassTest5.java
+++ src/test/java/com/Amazon/test/ClassTest5.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
+//updated
 public class ClassTest5 {
 @Test
  public void Radiobutton(){
