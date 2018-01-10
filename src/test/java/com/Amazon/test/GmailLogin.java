a5cd0205700d6156e81f8b70959cbfbd5cacbced
Bibek0914 <bibek.mallik14@gmail.com>
2018-01-10 23:43:25 +0530

    gg

--- src/test/java/com/Amazon/test/GmailLogin.java
+++ src/test/java/com/Amazon/test/GmailLogin.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
-///updated
+
 public class GmailLogin {
 @Test
  public void GmailUser(){
